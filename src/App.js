b8e048fcf7289e53781615a542dbac72ef8f2305
Kair <kair@MacBook-Air-Kair.local>
2023-12-19 17:52:49 +0600

    Added a project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/footer';
+import Header from './components/header';
+import VacancyComponent from './components/finder';
+import Main from './components/video';
+import Middle from './components/middle';
+import ResumeForm from './components/resume';
+import Home from './components/home'
+import About from './pages/about'
+import Contacts from './pages/contacts'
+
+const App = () => (
+  <div>
+    <Header />
+    <Routes>
+      <Route path="/find" element={<VacancyComponent />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/resume" element={<ResumeForm />} />
+      <Route path="/about" element={<About />}/>
+      <Route path="/contacts" element={<Contacts /> }/>
+    </Routes>
+    <Footer />
+  </div>
+);
+
+
+
+
 
 export default App;
+
+
